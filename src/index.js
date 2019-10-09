e4fce554b654f8653a5f33ccda940ce56db50840
Kirill Mager <kiruha24@mail.ru>
2019-10-10 02:45:37 +0300

    feat: passed all tests

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let codedString = new String(expr);
+    
+    let LettersArray = [];
+    let decodedString = '';
+
+    let letter = 0;
+    for (let i = 0; i < codedString.length; i += 10) {
+        codedSymbol = codedString.substr(i, 10);
+        codedSymbol = getPunctuationMarks(codedSymbol);
+        LettersArray.push(codedSymbol);
+        LettersArray[letter] == ' ' ? decodedString += ' ' : decodedString += MORSE_TABLE[LettersArray[letter]];
+        letter++;
+    }
+    
+    function getPunctuationMarks(encryptedString){
+        encryptedString = encryptedString.replace(/\*{10}/g, ' ');
+        encryptedString = encryptedString.replace(/00/g, "");
+        encryptedString = encryptedString.replace(/10/g, ".");
+        encryptedString = encryptedString.replace(/11/g, "-");
+        return encryptedString;
+    }
+    
+    return decodedString;
 }
 
 module.exports = {
